Fixed inlet/outlet pressure calculation

src/calculate_flux.py
@@ -110,7 +110,8 @@ def update_flux(state_vector):
 
     T_in = c_in**2 / (HEAT_CAPACITY_RATIO * GAS_CONSTANT)
     
-    rho_in = np.power(gv.rho_infty**HEAT_CAPACITY_RATIO * GAS_CONSTANT * T_in / ATMOSPHERIC_PRESSURE ,1/(HEAT_CAPACITY_RATIO-1))
+    #rho_in = np.power(gv.rho_infty**HEAT_CAPACITY_RATIO * GAS_CONSTANT * T_in / ATMOSPHERIC_PRESSURE ,1/(HEAT_CAPACITY_RATIO-1))
+    rho_in = np.power(gv.rho_infty**HEAT_CAPACITY_RATIO * GAS_CONSTANT * T_in / gv.p_0, 1/(HEAT_CAPACITY_RATIO-1))
 
     p_in = rho_in * GAS_CONSTANT * T_in
 
@@ -122,7 +123,8 @@ def update_flux(state_vector):
     gv.F[0,:,3,3] = rho_in * u_in * H_in * gv.ndS[0, :, 3, 0]        # Energy flux in x-direction
 
     # east flux at outlet
-    p_out = ATMOSPHERIC_PRESSURE * np.ones(NUM_CELLS_Y, 'd')
+    #p_out = ATMOSPHERIC_PRESSURE * np.ones(NUM_CELLS_Y, 'd')
+    p_out = gv.p_0 * np.ones(NUM_CELLS_Y, 'd')
     
     rho_out = gv.rho[-1, :] * np.power((p_out[:] / gv.p[-1, :]), 1/HEAT_CAPACITY_RATIO)
 

src/cell.py
@@ -37,16 +37,16 @@ def initialize():
 def calculate_inlet_properties():
 
     # # stagnation temperature
-    T_0 = ATMOSPHERIC_TEMPERATURE * (1 + (HEAT_CAPACITY_RATIO - 1)/2 * UPSTREAM_MACH_NUMBER**2)
+    gv.T_0 = ATMOSPHERIC_TEMPERATURE * (1 + (HEAT_CAPACITY_RATIO - 1)/2 * UPSTREAM_MACH_NUMBER**2)
     # # stagnation pressure
-    p_0 = ATMOSPHERIC_PRESSURE * (1 + (HEAT_CAPACITY_RATIO - 1)/2 * UPSTREAM_MACH_NUMBER**2)**(HEAT_CAPACITY_RATIO/(HEAT_CAPACITY_RATIO-1))
+    gv.p_0 = ATMOSPHERIC_PRESSURE * (1 + (HEAT_CAPACITY_RATIO - 1)/2 * UPSTREAM_MACH_NUMBER**2)**(HEAT_CAPACITY_RATIO/(HEAT_CAPACITY_RATIO-1))
 
-    # T_0 = ATMOSPHERIC_TEMPERATURE
-    # p_0 = ATMOSPHERIC_PRESSURE
+    #T_0 = ATMOSPHERIC_TEMPERATURE
+    #p_0 = ATMOSPHERIC_PRESSURE
 
-    gv.rho_infty[:] = p_0 / (GAS_CONSTANT * T_0)
+    gv.rho_infty[:] = gv.p_0 / (GAS_CONSTANT * gv.T_0)
 
-    gv.c_infty[:] = np.sqrt(HEAT_CAPACITY_RATIO * GAS_CONSTANT * T_0)
+    gv.c_infty[:] = np.sqrt(HEAT_CAPACITY_RATIO * GAS_CONSTANT * gv.T_0)
     
     gv.u_infty[:] = UPSTREAM_MACH_NUMBER * gv.c_infty
     return None