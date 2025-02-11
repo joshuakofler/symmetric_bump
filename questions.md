dd0134cb49e75e1102dbf8227f83e80b29a524dc
Joshua Kofler <joshua.kofler@hotmail.com>
2025-02-11 01:59:58 +0100

    Removed a question file and modified gitignore

--- questions.md
+++
@@ -1,3 +0,0 @@
-When defining the mesh, y₀ is not a linear function, so the center coordinates (based on the intersection of the diagonals) do not align with dy/2 or dx/2.
-
-Should we use the center coordinates calculated as dy/2 and dx/2, or those derived from the intersection of the diagonals?
--- src/test_plot.py
+++ src/test_plot.py
@@ -1,3 +1,29 @@
+#%%
+
+import pyvista as pv
 import numpy as np
-from matplotlib import pyplot as plt
+
+# Load the VTK file#
+reader = pv.VTKDataSetReader("..//output//contour_final.vtk")
+
+mesh = reader.read()
+
+#mesh = pv.read("..//output//contour_final.vtk")
+
+print(mesh)
+
+print(mesh['Mach Number'])
+print(mesh.points)
+
+
+# Create a PyVista mesh from the coordinates and scalar field
+
+points = np.column_stack((X, Y, Z))  # Combine X, Y, Z into a (N_points, 3) array
+mesh_with_scalars = pv.PolyData(points)
+mesh_with_scalars['Mach Number'] = mesh['Mach Number']  # Assign Mach Number as a scalar field
+
+# Plot the mesh with the scalar field
+mesh_with_scalars.plot(scalars='Mach Number', cmap='viridis', show_edges=True)
+
+
 
