d1eeff80a44e5b0be43cb5fb05163072442bd60e
Joshua Kofler <joshua.kofler@hotmail.com>
2025-02-16 14:24:18 +0100

    Added docstring and inline comments

--- src/calculate_residual.py
+++ src/calculate_residual.py
@@ -1,23 +1,43 @@
-# WIP
 # TODO: Done
-from constants import *
-import global_vars as gv
+
+# Import global variables, and modules
+import global_vars as gv  # Global variables used across the simulation
 import numpy as np
-import calculate_flux as cF
+
+from calculate_flux import update_flux
 
 def update_residual(s_vector):
-    # update flux vector using the currently saved cell properties
-    cF.update_flux(s_vector)
-    
-    for i, j in np.ndindex(NUM_CELLS_X, NUM_CELLS_Y):
-        gv.R[i, j, 0] = np.sum(gv.F_star[i, j, :, 0])
-        gv.R[i, j, 1] = np.sum(gv.F_star[i, j, :, 1])
-        gv.R[i, j, 2] = np.sum(gv.F_star[i, j, :, 2])
-        gv.R[i, j, 3] = np.sum(gv.F_star[i, j, :, 3])
+    """
+    Updates the global residual array `gv.R` based on the flux calculations.
 
-    # gv.R[:,:,0] = np.sum(gv.F[:,:,:,0], axis=2)
-    # gv.R[:,:,1] = np.sum(gv.F[:,:,:,1], axis=2)
-    # gv.R[:,:,2] = np.sum(gv.F[:,:,:,2], axis=2)
-    # gv.R[:,:,3] = np.sum(gv.F[:,:,:,3], axis=2)
+    This function updates the flux vector using the current cell properties 
+    (via `calculate_flux.update_flux`) and then computes the sum of fluxes 
+    along the third axis of the `gv.F_star` array to populate the residual 
+    array `gv.R`.
+
+    Parameters:
+    -----------
+    s_vector : numpy.ndarray
+        The state vector containing the current properties of the simulation 
+        (e.g., density, velocity, etc.).
+
+    Returns:
+    --------
+    None
+        The function directly updates the global variable `gv.R` in-place.
+    """
+    # Update the flux vector based on the current state vector
+    update_flux(s_vector)
+
+    # Compute the residual using a vectorized operation
+    # Sum over the third axis (axis=2) of F_star to populate gv.R.
+    gv.R[:] = np.sum(gv.F_star, axis=2)
+
+    # The following is a loop-based implementation for reference, which is less efficient:
+    # for i, j in np.ndindex(NUM_CELLS_X, NUM_CELLS_Y):
+    #     gv.R[i, j, 0] = np.sum(gv.F_star[i, j, :, 0])
+    #     gv.R[i, j, 1] = np.sum(gv.F_star[i, j, :, 1])
+    #     gv.R[i, j, 2] = np.sum(gv.F_star[i, j, :, 2])
+    #     gv.R[i, j, 3] = np.sum(gv.F_star[i, j, :, 3])
 
     return None
